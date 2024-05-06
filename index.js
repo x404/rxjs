dff381e6539cad167e5a9c34f5007abceff456d4
x404 <kronos2003@gmail.com>
2024-05-06 16:36:12 +0300

    added rxjs lib

--- index.js
+++ index.js
@@ -8,15 +8,22 @@
 //
 // sequence.then( v => console.log(v));
 // sequence.then( v => console.log(v));
+Object.defineProperty(exports, "__esModule", { value: true });
 // GENERATORS
-const sequence = function* iteratorFn() {
-    let item = 1;
-    while (true) {
-        yield item++;
-    }
-}();
-console.log(sequence.next().value);
-console.log(sequence.next().value);
-console.log(sequence.next().value);
-console.log(sequence.next().value);
-console.log(sequence.next().value);
+// const sequence = function* iteratorFn () {
+//     let item = 1;
+//     while (true){
+//         yield item++;
+//     }
+// }();
+//
+// console.log(sequence.next().value);
+// console.log(sequence.next().value);
+// console.log(sequence.next().value);
+// console.log(sequence.next().value);
+// console.log(sequence.next().value);
+const rxjs_1 = require("rxjs");
+(0, rxjs_1.interval)(1000)
+    .subscribe((v) => {
+    console.log(v);
+});
