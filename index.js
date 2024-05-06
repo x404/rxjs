b7bcc0a20a7cbb2cfab003541f08f054824647bf
x404 <kronos2003@gmail.com>
2024-05-06 16:32:29 +0300

    added generators

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 "use strict";
-const sequence = new Promise(res => {
-    let count = 1;
-    setInterval(() => {
-        res(count++);
-    });
-});
-sequence.then(v => console.log(v));
-sequence.then(v => console.log(v));
+// const sequence = new Promise (res => {
+//     let count = 1;
+//     setInterval(()=>{
+//         res(count++)
+//     })
+// });
+//
+// sequence.then( v => console.log(v));
+// sequence.then( v => console.log(v));
+// GENERATORS
+const sequence = function* iteratorFn() {
+    let item = 1;
+    while (true) {
+        yield item++;
+    }
+}();
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
--- index.ts
+++ index.ts
@@ -1,9 +1,24 @@
-const sequence = new Promise (res => {
-    let count = 1;
-    setInterval(()=>{
-        res(count++)
-    })
-});
+// const sequence = new Promise (res => {
+//     let count = 1;
+//     setInterval(()=>{
+//         res(count++)
+//     })
+// });
+//
+// sequence.then( v => console.log(v));
+// sequence.then( v => console.log(v));
 
-sequence.then( v => console.log(v));
-sequence.then( v => console.log(v));
+
+// GENERATORS
+const sequence = function* iteratorFn () {
+    let item = 1;
+    while (true){
+        yield item++;
+    }
+}();
+
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
