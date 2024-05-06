0839cb3117e6b9a6c773b8fdfe4600b90ccee25f
x404 <kronos2003@gmail.com>
2024-05-06 16:20:00 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+const sequence = new Promise(res => {
+    let count = 1;
+    setInterval(() => {
+        res(count++);
+    });
+});
+sequence.then(v => console.log(v));
+sequence.then(v => console.log(v));
